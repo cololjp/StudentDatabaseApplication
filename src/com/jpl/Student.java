adc7ed2835a5b338fcf4a39f3bcb3312ed3ffcd5
Juan Pablo L <colo.ljp@hotmail.com>
2018-04-18 00:25:57 -0300

    Minor tweaks

--- src/com/jpl/Student.java
+++ src/com/jpl/Student.java
@@ -1,28 +1,41 @@
 package com.jpl;
 
+import org.apache.commons.text.WordUtils;
+
 public class Student {
 
     private String firstName;
     private String lastName;
-    private int id;
+    private String studentId;
     private GradeYear gradeYear;
     private String courses;
     private int balance;
 
+    private static int id_counter = 1000;
     private static final int COURSE_COST = 500;  // Dollars
 
     private Student() { }
 
     public Student(String firstName, String lastName, GradeYear gradeYear, String courses) {
-        this.firstName = firstName;
-        this.lastName = lastName;
+        this.firstName = WordUtils.capitalize(firstName);
+        this.lastName = WordUtils.capitalize(lastName);
         this.gradeYear = gradeYear;
         this.courses = courses;
+        this.studentId = setStudentId();
+    }
+
+    private String setStudentId() {
+        // Grade year + ID
+        String tempStudentId = gradeYear.getValue() + String.valueOf(id_counter);
+        id_counter++;     // Increment the ID so every Student has a different one
+
+        return tempStudentId;
     }
 
     @Override
     public String toString() {
         return "\nFirst Name: " + firstName + "\nLast Name: " + lastName +
+                "\nStudent ID: " + studentId +
                 "\nGrade Year: " + gradeYear.getValue() +
                 "\nCourses: " + courses;
     }
